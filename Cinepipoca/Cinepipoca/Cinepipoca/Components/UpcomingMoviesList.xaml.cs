eb9210b3d1e91e8984947a975bbef404442cf8be
Thiago H <thiago.hpereira@mrv.com.br>
2019-04-15 17:40:49 -0300

    integration of viewmodels and data

---
+++ Cinepipoca/Cinepipoca/Cinepipoca/Components/UpcomingMoviesList.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace Cinepipoca.Components
+{
+    public partial class UpcomingMoviesList : ContentView
+    {
+        public static readonly BindableProperty FooProperty =
+            BindableProperty.Create(
+                nameof(Foo),
+                typeof(string),
+                typeof(UpcomingMoviesList), null);
+                
+
+        public UpcomingMoviesList()
+        {
+            InitializeComponent();
+        }
+
+        public string Foo
+        {
+            get => (string)GetValue(FooProperty);
+            set => SetValue(FooProperty, value);
+        }
+    }
+}
